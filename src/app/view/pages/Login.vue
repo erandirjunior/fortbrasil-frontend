b2d9702d6ae04b292e20dcc3c14328904a60c34a
Erandir Junior <aefs12junior@gmail.com>
2020-09-21 16:19:39 -0300

    Created Login page

---
+++ src/app/view/pages/Login.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-page class="q-pa-xl">
+    <q-card class="card-content">
+      <q-card-section>
+        Login
+      </q-card-section>
+      <q-card-section>
+        Form
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import handlerActionMixin from 'mixins/handlerActionMixin'
+
+export default {
+  name: 'Login',
+  mixins: [
+    handlerActionMixin
+  ],
+  methods: {
+    addFile (evt) {
+      console.log(evt.dataTransfer)
+      evt.preventDefault()
+      this.file = evt.dataTransfer.files[0]
+      this.show = !this.show
+    }
+  }
+}
+</script>
+
+<style>
+  .card-content {
+    width: 35%;
+    margin: 10% auto;
+  }
+</style>

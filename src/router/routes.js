bb4a17fad9de1393d177da07132e2423884489b1
Erandir <aefs12junior@gmail.com>
2020-09-28 15:24:06 -0300

    Routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,24 +4,30 @@ const routes = [
     path: '/',
     component: () => import('src/app/view/layouts/HomeLayout.vue'),
     children: [
-      { path: '', component: () => import('src/app/view/pages/Home.vue') }
+      {
+        path: '',
+        component: () => import('src/app/view/pages/Home.vue'),
+        meta: {
+          Authorization: true
+        }
+      },
+      {
+        path: '/account',
+        component: () => import('src/app/view/pages/Account.vue'),
+        meta: {
+          Authorization: true
+        }
+      }
     ]
   },
   {
-    path: '/login',
+    path: '/',
     component: () => import('src/app/view/layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('src/app/view/pages/Login.vue') }
+      { path: 'login', component: () => import('src/app/view/pages/Login.vue') },
+      { path: 'register', component: () => import('src/app/view/pages/Register.vue') }
     ]
   }
 ]
-
-// Always leave this as last one
-if (process.env.MODE !== 'ssr') {
-  routes.push({
-    path: '*',
-    component: () => import('src/app/view/pages/Error404.vue')
-  })
-}
 
 export default routes

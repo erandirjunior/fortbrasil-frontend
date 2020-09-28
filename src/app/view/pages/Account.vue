298c16de32047865cb8797546b5dfb892c1588d7
Erandir <aefs12junior@gmail.com>
2020-09-28 15:19:03 -0300

    Create accout page

---
+++ src/app/view/pages/Account.vue
@@ -0,0 +1,129 @@
+<template>
+  <q-page class="q-pa-xl">
+    <q-card class="card-content">
+      <q-card-section class="text-weight-bold text-h5">
+        Informações Pessoais
+      </q-card-section>
+      <q-separator />
+      <q-card-section>
+        <form-factory
+          class-factory="row q-col-gutter-sm"
+          :form="form"
+          :fields="accountForm"
+          :validation="$v"
+          @formAction="action"
+        />
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import handlerActionMixin from 'mixins/handlerActionMixin'
+import FormFactory from 'components/general/form/FormFactory'
+import Account from 'builder/forms/Account'
+import { required, email } from 'vuelidate/lib/validators'
+import alert from 'src/app/infrastructure/components/alert/alert'
+import { get, put } from 'src/app/infrastructure/components/request/request'
+
+export default {
+  name: 'Account',
+  mixins: [
+    handlerActionMixin
+  ],
+  components: {
+    FormFactory
+  },
+  data () {
+    return {
+      accountForm: new Account(),
+      form: {
+        id: '',
+        name: '',
+        email: '',
+        password: ''
+      }
+    }
+  },
+  validations: {
+    form: {
+      name: {
+        required
+      },
+      email: {
+        required,
+        email
+      }
+    }
+  },
+  methods: {
+    loadData () {
+      const id = localStorage.getItem('user')
+      get(`users/${id}`)
+        .then((response) => response.data)
+        .then((response) => {
+          this.form.email = response.email
+          this.form.name = response.name
+          this.form.id = id
+        })
+    },
+    update () {
+      this.$v.$touch()
+
+      if (this.$v.form.$error) {
+        const msg = 'Por favor, verifique os campos do formulário e tente novamente!'
+        return alert(msg, 'red', 'thumb_down')
+      }
+
+      put(`users/${this.form.id}`, this.form)
+        .then(() => {
+          alert('Sucesso ao atualizar dados!', 'positive', 'thumb_up')
+          this.$router.push('/')
+        })
+        .catch(error => {
+          if (Array.isArray(error.response.data)) {
+            error.response.data.forEach(msg => {
+              alert(msg, 'red', 'thumb_down')
+            })
+
+            return
+          }
+
+          alert(error, 'red', 'thumb_down')
+        })
+    }
+  },
+  created () {
+    this.loadData()
+  }
+}
+</script>
+
+<style>
+  .card-content {
+    width: 30%;
+    margin: 2% auto;
+  }
+  @media screen and (max-width: 400px) {
+    .card-content {
+      width: 100%;
+    }
+  }
+  .button-color {
+    background: #00b4db;
+    background: linear-gradient(to right, #00b4db, #0083b0);
+  }
+  .button-text-color {
+    background: -webkit-linear-gradient(#00b4db, #0083b0);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .text-brand {
+    background: -webkit-linear-gradient(#00b4db, #0083b0);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+  .bg-brand {
+    background: #a2aa33;
+  }
+</style>

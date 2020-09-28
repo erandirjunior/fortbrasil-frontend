58ea63eb5ef53888ab2f53ce71215c58b4d53594
Erandir <aefs12junior@gmail.com>
2020-09-28 15:21:40 -0300

    Created Account form

---
+++ src/app/infrastructure/builder/forms/Account.js
@@ -0,0 +1,45 @@
+import FormBuilder from 'src/app/infrastructure/builder/forms/FormBuilder'
+import Input from 'src/app/infrastructure/components/fields/Input'
+import Button from 'src/app/infrastructure/components/fields/Button'
+
+export default class Account extends FormBuilder {
+  createFields () {
+    this.addField(new Input()
+      .setLabel('Nome')
+      .setModel('name')
+      .setColumnClass('col-xs-12')
+      .setFilled(true)
+      .setAutofocus(true)
+      .setHasPrepend(true)
+      .setPrependIcon('person')
+      .setAutofocus(true)
+    )
+
+    this.addField(new Input()
+      .setLabel('E-mail')
+      .setModel('email')
+      .setColumnClass('col-xs-12')
+      .setFilled(true)
+      .setAutofocus(true)
+      .setHasPrepend(true)
+      .setPrependIcon('email')
+    )
+
+    this.addField(new Input()
+      .setLabel('Senha')
+      .setModel('password')
+      .setType('password')
+      .setColumnClass('col-xs-12')
+      .setFilled(true)
+      .setHasPrepend(true)
+      .setPrependIcon('vpn_key')
+    )
+
+    this.addField(new Button()
+      .setLabel('Atualizar')
+      .setColumnClass('col-xs-12')
+      .setClass('full-width button-color')
+      .setClick('update')
+    )
+  }
+}

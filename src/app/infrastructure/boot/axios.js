6fdf546ce1300fdc54b686754821f341bb7f68d3
Erandir <aefs12junior@gmail.com>
2020-09-28 15:21:03 -0300

    applied improvements in axios

--- src/app/infrastructure/boot/axios.js
+++ src/app/infrastructure/boot/axios.js
@@ -1,4 +1,14 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+axios.interceptors.request.use(function (config) {
+  const regex = config.url.match(/https:\/\/viacep\.com\.br\/ws\//)
+
+  if (!regex) {
+    config.headers.Authorization = localStorage.getItem('tokenSite')
+  }
+
+  return config
+})
+
 Vue.prototype.$axios = axios
